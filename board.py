52aac47c6255a1fa0d4e67e4bbe3c4e753d5834c
sandrinedacol <sandrine.dacol@gmail.com>
2024-11-27 10:13:41 +0100

    replace piece to pawn

--- board.py
+++ board.py
@@ -5,12 +5,12 @@ class Board():
     def __init__(self):
         self.df = pd.DataFrame(index = list(range(1,9)), columns = list('ABCDEFGH'))
 
-    def add_piece(self, piece):
-        if piece.color:
+    def add_pawn(self, pawn):
+        if pawn.color:
             marker = 'O'
         else:
             marker = 'X'
-        self.df.at[piece.index, piece.column] = marker
+        self.df.at[pawn.index, pawn.column] = marker
 
     def __str__(self):
         return self.df
--- game.py
+++ game.py
@@ -1,34 +1,34 @@
 from board import *
-from piece import *
+from pawn import *
 
 class Game():
 
     def __init__(self):
         self.in_progress = True
         self.board = Board()
-        self.all_pieces = [Piece(i) for i in range(1,65)]
+        self.all_pawns = [Pawn(i) for i in range(1,65)]
         self.step = 0
         self.player = False # False si noir True si blanc
-        self.add_initial_pieces()
+        self.add_initial_pawns()
         print(self.board.df)
 
-    def add_initial_pieces(self):
+    def add_initial_pawns(self):
         for position in ['d4', 'e4', 'e5', 'd5']:
-            self.put_piece_on_board(position)
+            self.put_pawn_on_board(position)
             self.step += 1
             self.player = not self.player
 
-    def put_piece_on_board(self, position):
-        piece = self.all_pieces[self.step]
-        piece.add_on_board(position, self.player)
-        self.board.add_piece(piece)
+    def put_pawn_on_board(self, position):
+        pawn = self.all_pawns[self.step]
+        pawn.add_on_board(position, self.player)
+        self.board.add_pawn(pawn)
         if self.step > 3:
             print(self.board.df)
         
     def play_next_step(self, position):
         self.verify_position(position)
-        self.put_piece_on_board(position)
-        self.turn_pieces_over()
+        self.put_pawn_on_board(position)
+        self.turn_pawns_over()
         self.check_end_game()
         self.step += 1
         self.player = not self.player
@@ -36,7 +36,7 @@ class Game():
     def verify_position(self, position):
         pass
 
-    def turn_pieces_over(self):
+    def turn_pawns_over(self):
         pass
 
     def check_end_game(self):
